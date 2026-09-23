bb61af054a99eaeddfc66bf376ac05d2de780122
Fajri Pangestu <fajripangestu62@gmail.com>
2026-09-23 14:23:31 +0700

    Add seed data 2

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { UsersRepository } from './users.repository';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [UsersController],
   providers: [UsersService, UsersRepository],
 })
