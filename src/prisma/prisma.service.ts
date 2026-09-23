fd3d1ec02feebe0df7fa0269d8f0f8b615e6e964
Fajri Pangestu <fajripangestu62@gmail.com>
2026-09-23 15:28:43 +0700

    Fix postgres ssl connection

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -6,7 +6,10 @@ import { PrismaClient } from 'generated/prisma/client';
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy{
     constructor(){
         const adapter = new PrismaPg({
-            connectionString: process.env.DATABASE_URL
+            connectionString: process.env.DATABASE_URL,
+            ssl: {
+                rejectUnauthorized: false,
+            },
         });
         super({adapter});
     }
