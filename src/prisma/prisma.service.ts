a6f75f864399df0df62e2efeb7a11b7b7e676135
Fajri Pangestu <fajripangestu62@gmail.com>
2026-09-25 15:49:59 +0700

    Change to supabase

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -32,9 +32,7 @@ export class PrismaService extends PrismaClient {
   constructor() {
     const adapter = new PrismaPg({
       connectionString: process.env.DATABASE_URL,
-      ssl: {
-        rejectUnauthorized: false,
-      },
+      ssl: { rejectUnauthorized: false },
     });
 
     super({
